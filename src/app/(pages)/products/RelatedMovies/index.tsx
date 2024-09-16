f767272895284e7265a2477d14d8ebde88137e8a
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-09-16 17:06:34 +0200

    I added stripe to product page

--- src/app/(pages)/products/RelatedMovies/index.tsx
+++ src/app/(pages)/products/RelatedMovies/index.tsx
@@ -34,32 +34,35 @@ export const RelatedMovies: React.FC<{}> = () => {
   const catLabels = ['Growkit 1', 'Growkit 2', 'Growkit 3', 'Growkit 4', 'Growkit 5']
 
   return (
-    <Gutter className={classes.relatedGradient}>
-      {/*Related */}
-      <section>
-        <div className="mt-8 border-solid border-b-0 border-r-0 border-t-1 border-l-1 border-primary pl-5 py-4">
-          <h6 className="text-primary text-4xl font-medium">Related</h6>
+    <>
+      <div className={classes.diagonalSection}></div>
+      <Gutter className={classes.relatedGradient}>
+        {/*Related */}
+        <section>
+          <div className="mt-8 border-solid border-b-0 border-r-0 border-t-1 border-l-1 border-primary pl-5 py-4">
+            <h6 className="text-primary text-4xl font-medium">Related</h6>
+          </div>
+          <RelatedCarousel icons={null} images={images2} catLabels={catLabels} />
+
+        </section>
+
+        {/*Movies */}
+        <section>
+          <div className="mb-10 mt-6 border-solid border-b-0 border-r-0 border-t-1 border-l-1 border-primary pl-5 py-4">
+            <h6 className="text-primary text-4xl font-medium">Movies</h6>
+          </div>
+          <NewsCarousel catLabels={null} images={images2} />
+        </section>
+
+        {/* hr */}
+        <div className="bg-transparent mt-10 pb-3">
+          <hr className="border-l-gray-100 w-1/3 opacity-30 py-0 my-0" />
         </div>
-        <RelatedCarousel icons={null} images={images2} catLabels={catLabels} />
 
-      </section>
-
-      {/*Movies */}
-      <section>
-        <div className="mb-10 mt-6 border-solid border-b-0 border-r-0 border-t-1 border-l-1 border-primary pl-5 py-4">
-          <h6 className="text-primary text-4xl font-medium">Movies</h6>
+        <div onClick={scrollToTop} className="flex justify-end mt-10">
+          <FaArrowAltCircleUp className="text-8xl text-primary" />
         </div>
-        <NewsCarousel catLabels={null} images={images2} />
-      </section>
-
-      {/* hr */}
-      <div className="bg-transparent mt-10 pb-3">
-        <hr className="border-l-gray-100 w-1/3 opacity-30 py-0 my-0" />
-      </div>
-
-      <div onClick={scrollToTop} className="flex justify-end mt-10">
-        <FaArrowAltCircleUp className="text-8xl text-primary" />
-      </div>
-    </Gutter>
+      </Gutter>
+    </>
   )
 }
