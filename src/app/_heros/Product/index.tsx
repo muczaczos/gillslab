9bb0facb3a185fef0abf5d20be211acc7f177023
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-10-14 22:57:15 +0200

    I added styles in product hero for bigger screens

--- src/app/_heros/Product/index.tsx
+++ src/app/_heros/Product/index.tsx
@@ -68,18 +68,19 @@ export const ProductHero: React.FC<{
   const websiteUrl = process.env.NEXT_PUBLIC_SERVER_URL
   return (
     <>
+    <div className="lg:flex">
       {/* main image with buttons */}
-      <section className="bg-primary pt-5 pb-16">
-        <div className="flex justify-between px-5 pt-5 pb-7">
+      <section className="bg-primary pt-5 pb-16 lg:w-1/2">
+        <div className="flex justify-between px-5 pt-5 pb-7 lg:mb-20">
           <div>
-            <IoArrowBackOutline className="text-3xl text-customWhite md:text-6xl" />
+            <IoArrowBackOutline className="text-3xl text-customWhite md:text-6xl lg:text-5xl" />
           </div>
           <div>
-            <IoShareOutline className="text-3xl text-customWhite mr-5 md:text-6xl" />
-            <IoHeartOutline className="text-3xl text-customWhite md:text-6xl" />
+            <IoShareOutline className="text-3xl text-customWhite mr-5 md:text-6xl lg:text-5xl" />
+            <IoHeartOutline className="text-3xl text-customWhite md:text-6xl lg:text-5xl" />
           </div>
         </div>
-        <div className="flex justify-center">
+        <div className="flex justify-center items-center">
           <div className="w-1/2 sm:w-3/4 ">
             <Image
               alt="Product Image"
@@ -93,12 +94,12 @@ export const ProductHero: React.FC<{
       </section>
 
       {/* hr */}
-      <div className="bg-customWhite pt-5 pb-3 md:pt-10 md:pb-8">
+      <div className="lg:hidden bg-customWhite pt-5 pb-3 md:pt-10 md:pb-8">
         <hr className="border-l-gray-100 w-1/3 opacity-30 py-0 my-0" />
       </div>
 
       {/* titles, short descriptions, price and addToCart button */}
-      <section className="px-5 bg-customWhite pb-5">
+      <section className="px-5 bg-customWhite pb-5 lg:w-1/2 lg:pt-12">
         <h3 className="text-primary text-2xl md:text-4xl">{title}</h3>
         <p className="text-primary-light text-xl md:text-2xl">{title2}</p>
 
@@ -110,15 +111,40 @@ export const ProductHero: React.FC<{
           className="mb-10 text-lg text-customGray-dark font-medium md:text-2xl"
           content={productDescription.richText}
         />
-        <SmallCarousel icons={icons} catLabels={infoLabels} images={null} />
+        <div className="lg:hidden">
+          <SmallCarousel icons={icons} catLabels={infoLabels} images={null}/>
+        </div>
+        <div className="hidden lg:flex lg:flex-col lg:gap-5 2xl:flex-row 2xl:gap-0 justify-center pb-10 2xl:pb-28">
+          <div className="flex justify-center">
+            <div className="ml-4 flex gap-2 p-2 bg-white flex-col justify-center items-center shadow-[4px_4px_0px_rgba(0,0,0,0.2)] min-w-[45%] 2xl:min-w-[40%] transition-transform duration-300 ease-in-out rounded-xl h-[140px]">
+              <GiWateringCan size={50} color="#4968AC" />
+              <h3 className="text-xs text-primary opacity-70 md:text-lg">"Watering needed"</h3>
+            </div>
+            <div className="ml-4 flex gap-2 p-2 bg-white flex-col justify-center items-center shadow-[4px_4px_0px_rgba(0,0,0,0.2)] min-w-[45%] 2xl:min-w-[40%] transition-transform duration-300 ease-in-out rounded-xl h-[140px]">
+              <GiWateringCan size={50} color="#4968AC" />
+              <h3 className="text-xs text-primary opacity-70 md:text-lg">"Watering needed"</h3>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <div className="ml-4 flex gap-2 p-2 bg-white flex-col justify-center items-center shadow-[4px_4px_0px_rgba(0,0,0,0.2)] min-w-[45%] 2xl:min-w-[40%] transition-transform duration-300 ease-in-out rounded-xl h-[140px]">
+              <GiWateringCan size={50} color="#4968AC" />
+              <h3 className="text-xs text-primary opacity-70 md:text-lg">"Watering needed"</h3>
+            </div>
+            <div className="ml-4 flex gap-2 p-2 bg-white flex-col justify-center items-center shadow-[4px_4px_0px_rgba(0,0,0,0.2)] min-w-[45%] 2xl:min-w-[40%] transition-transform duration-300 ease-in-out rounded-xl h-[140px]">
+              <GiWateringCan size={50} color="#4968AC" />
+              <h3 className="text-xs text-primary opacity-70 md:text-lg">"Watering needed"</h3>
+            </div>
+          </div>
+        </div>
         <div className="mt-5 flex justify-between items-center pb-5">
-          <p className="text-primary text-5xl font-medium md:text-8xl">{`€${price}`}</p>
+          <p className="text-primary text-5xl font-medium md:text-8xl lg:text-6xl">{`€${price}`}</p>
           <AddToCartButton
             product={product}
-            className="bg-secondary px-3 flex h-16 items-center text-customWhite font-bold md:h-24"
+            className="bg-secondary px-3 flex h-16 items-center text-customWhite font-bold md:h-24 lg:h-16 lg:px-1"
           />
         </div>
       </section>
+      </div>
     </>
   )
 }
