9a5e8908b010fc5f43f655073b538e17ee051a2c
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-10-18 11:39:27 +0200

    I fited footer in bigger screen size

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -71,77 +71,81 @@ const FooterComponent = ({ footer }: { footer: Footer }) => {
             backgroundImage: `url(${process.env.NEXT_PUBLIC_SERVER_URL}/media/eyes5.png)`,
           }}
         >
-          {/* Address, Socials, Payments, Shipping*/}
-          <div className="flex xs:justify-between csm:gap-20 sm:gap-5 justify-center lg:justify-around sm:mb-10">
-            <div className="flex flex-col sm:flex-row">
-              {/* Logo and company details */}
-              <div className="flex flex-col lg:flex-row lg:gap-5">
-                <section className="mb-5">
-                  <Image
-                    src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/logo3.png`}
-                    alt="Gillslab logo"
-                    width="150"
-                    height="100"
-                    objectFit="cover"
-                    className="relative -left-[10px]"
-                  />
+          <div className="">
+            <div className="xxl:w-[2000px] xxl:mx-auto">
+              {/* Address, Socials, Payments, Shipping*/}
+              <div className="flex xs:justify-between csm:gap-20 sm:gap-5 justify-center lg:justify-around sm:mb-10">
+                <div className="flex flex-col sm:flex-row">
+                  {/* Logo and company details */}
+                  <div className="flex flex-col lg:flex-row lg:gap-5">
+                    <section className="mb-5">
+                      <Image
+                        src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/logo3.png`}
+                        alt="Gillslab logo"
+                        width="150"
+                        height="100"
+                        objectFit="cover"
+                        className="relative -left-[10px]"
+                      />
+                      <p className="text-customWhite text-xl">Gillslab LTD.</p>
+                      <p className="text-customWhite text-xl">05-500</p>
+                      <p className="text-customWhite text-xl">Piaseczno</p>
+                      <p className="text-customWhite text-xl">Szkolna 1/3</p>
+                      <p className="text-customWhite text-xl">Poland</p>
+                      <FaWhatsapp className="text-4xl text-customWhite mr-5" />{' '}
+                      <FaPhone className="text-3xl text-customWhite" />
+                    </section>
+
+                    {/* Socials */}
+                    <section className="mb-5 lg:mr-10">
+                      <h4 className="text-customWhite font-bold">Socials</h4>
+                      <div className="flex flex-row lg:flex-col">
+                        <FaFacebook className="text-4xl  mr-5 text-customWhite lg:mb-5" />
+                        <FaInstagram className=" text-4xl mr-5 text-customWhite lg:mb-5" />
+                        <FaVimeo className="text-4xl text-customWhite" />
+                      </div>
+                    </section>
+                  </div>
+                  <div className="flex flex-col md:flex-row md:gap-10">
+                    {/* Payments */}
+                    <section className="mb-5">
+                      <h4 className="text-customWhite font-bold">Payments</h4>
+                      <div className="flex flex-row lg:flex-col">
+                        <FaBtc className="text-4xl  mr-5 text-customWhite mb-5" />
+                        <FaCreditCard className=" text-4xl mr-5 text-customWhite mb-5" />
+                        <FaVimeo className="text-4xl text-customWhite" />
+                      </div>
+                    </section>
+
+                    {/* Shipping */}
+                    <section className="mb-5 lg:mr-5">
+                      <h4 className="text-customWhite font-bold">Shipping</h4>
+                      <p className="text-customWhite text-xl">Poland 0-1 Days</p>
+                      <p className="text-customWhite text-xl">Europe 1-3 Days</p>
+                      <p className="text-customWhite text-xl">USA 4-7 Days</p>
+                    </section>
+                  </div>
+                </div>
+                <section>
+                  <h4 className="text-customWhite font-bold">Sitemap</h4>
                   <p className="text-customWhite text-xl">Gillslab LTD.</p>
-                  <p className="text-customWhite text-xl">05-500</p>
-                  <p className="text-customWhite text-xl">Piaseczno</p>
-                  <p className="text-customWhite text-xl">Szkolna 1/3</p>
-                  <p className="text-customWhite text-xl">Poland</p>
-                  <FaWhatsapp className="text-4xl text-customWhite mr-5" />{' '}
-                  <FaPhone className="text-3xl text-customWhite" />
-                </section>
-
-                {/* Socials */}
-                <section className="mb-5 lg:mr-10">
-                  <h4 className="text-customWhite font-bold">Socials</h4>
-                  <div className="flex flex-row lg:flex-col">
-                    <FaFacebook className="text-4xl  mr-5 text-customWhite lg:mb-5" />
-                    <FaInstagram className=" text-4xl mr-5 text-customWhite lg:mb-5" />
-                    <FaVimeo className="text-4xl text-customWhite" />
-                  </div>
-                </section>
-              </div>
-              <div className="flex flex-col md:flex-row md:gap-10">
-                {/* Payments */}
-                <section className="mb-5">
-                  <h4 className="text-customWhite font-bold">Payments</h4>
-                  <div className="flex flex-row lg:flex-col">
-                    <FaBtc className="text-4xl  mr-5 text-customWhite mb-5" />
-                    <FaCreditCard className=" text-4xl mr-5 text-customWhite mb-5" />
-                    <FaVimeo className="text-4xl text-customWhite" />
-                  </div>
-                </section>
-
-                {/* Shipping */}
-                <section className="mb-5 lg:mr-5">
-                  <h4 className="text-customWhite font-bold">Shipping</h4>
-                  <p className="text-customWhite text-xl">Poland 0-1 Days</p>
-                  <p className="text-customWhite text-xl">Europe 1-3 Days</p>
-                  <p className="text-customWhite text-xl">USA 4-7 Days</p>
+                  <p className="text-customWhite text-xl">Growkits</p>
+                  <p className="text-customWhite text-xl">Spores</p>
+                  <p className="text-customWhite text-xl">Cultures</p>
+                  <p className="text-customWhite text-xl">Lab</p>
+                  <p className="text-customWhite text-xl">Substrates</p>
+                  <p className="text-customWhite text-xl">Contact</p>
+                  <p className="text-customWhite text-xl">Conditions</p>
+                  <p className="text-customWhite text-xl">Privacy</p>
+                  <p className="text-customWhite text-xl">Blog</p>
+                  <p className="text-customWhite text-xl">Vlog</p>
+                  <p className="text-customWhite text-xl">Shipping</p>
+                  <p className="text-customWhite text-xl">Payments</p>
+                  <p className="text-customWhite text-xl">Cart</p>
+                  <p className="text-customWhite text-xl">Favorities</p>
                 </section>
               </div>
             </div>
-            <section>
-              <h4 className="text-customWhite font-bold">Sitemap</h4>
-              <p className="text-customWhite text-xl">Gillslab LTD.</p>
-              <p className="text-customWhite text-xl">Growkits</p>
-              <p className="text-customWhite text-xl">Spores</p>
-              <p className="text-customWhite text-xl">Cultures</p>
-              <p className="text-customWhite text-xl">Lab</p>
-              <p className="text-customWhite text-xl">Substrates</p>
-              <p className="text-customWhite text-xl">Contact</p>
-              <p className="text-customWhite text-xl">Conditions</p>
-              <p className="text-customWhite text-xl">Privacy</p>
-              <p className="text-customWhite text-xl">Blog</p>
-              <p className="text-customWhite text-xl">Vlog</p>
-              <p className="text-customWhite text-xl">Shipping</p>
-              <p className="text-customWhite text-xl">Payments</p>
-              <p className="text-customWhite text-xl">Cart</p>
-              <p className="text-customWhite text-xl">Favorities</p>
-            </section>
           </div>
           <hr className="border-customWhite w-full sm:mb-10 xl:w-3/4" />
           {/* dhl, dpd, ups, fedex logotypes */}
