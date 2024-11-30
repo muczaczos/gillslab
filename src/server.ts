f61f1d6d9335d290de08d15cbcf10a1d4f31c13b
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-11-30 21:42:24 +0100

    another error on production - unused directive

--- src/server.ts
+++ src/server.ts
@@ -112,7 +112,6 @@ const start = async (): Promise<void> => {
   if (process.env.NEXT_BUILD) {
     app.listen(PORT, async () => {
       payload.logger.info(`Next.js is now building...`)
-      // @ts-expect-error
       await nextBuild(path.join(__dirname, '../'))
       process.exit()
     })
