d67ece4648cade75c155b83231519fdb74a19cdf
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-11-30 21:52:05 +0100

    error on production with nextBuild

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import dotenv from 'dotenv'
 import next from 'next'
-import nextBuild from 'next/dist/build'
 import path from 'path'
 
 dotenv.config({
@@ -107,17 +106,6 @@ const start = async (): Promise<void> => {
   if (process.env.PAYLOAD_SEED === 'true') {
     await seed(payload)
     process.exit()
-  }
-
-  if (process.env.NEXT_BUILD) {
-    app.listen(PORT, async () => {
-      payload.logger.info(`Next.js is now building...`)
-      // @ts-expect-error
-      await nextBuild(path.join(__dirname, '../'))
-      process.exit()
-    })
-
-    return
   }
 
   const nextApp = next({
