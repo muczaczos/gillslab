4ab2151f31f6e79343174fe77686d0471eb2d617
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-12-07 16:54:58 +0100

    Problem on production with compiling...

--- src/app/_heros/PostHero/index.tsx
+++ src/app/_heros/PostHero/index.tsx
@@ -21,10 +21,16 @@ export const PostHero: React.FC<{
     populatedAuthors,
   } = post
 
+  const imageSrc = process.env.NEXT_PUBLIC_SERVER_URL + '/media/' + post.meta.image.filename
+
   return (
     <>
       <div className={classes.heroImage}></div>
       <h1>H1 hero</h1>
+      <div className="p-3"></div>
+      <div className="bg-cover bg-center" style={{ backgroundImage: `blue` }}>
+        {/* Treść div */}
+      </div>
     </>
   )
 }
