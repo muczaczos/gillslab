9b23723a144780a11e9a4463f305caecde976db0
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-12-08 16:55:06 +0100

    I added rules to prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,8 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    'import/default': 'off', // Wyłącza regułę dla domyślnego importu
+    'no-unused-vars': 'warn', // Zmienia błąd na ostrzeżenie
+  },
 }
