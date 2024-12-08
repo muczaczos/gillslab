3aa011213902f12f5083204bca884bb6cb65aacb
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-12-08 16:47:46 +0100

    Initial server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
+import axios from 'axios'
 import dotenv from 'dotenv'
+import next from 'next'
+import nextBuild from 'next/dist/build'
 import path from 'path'
-
-// This file is used to replace `server.ts` when ejecting i.e. `yarn eject`
-// See `../eject.ts` for exact details on how this file is used
-// See `./README.md#eject` for more information
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
@@ -17,9 +16,63 @@ import { seed } from './payload/seed'
 const app = express()
 const PORT = process.env.PORT || 3000
 
-// Redirect root to the admin panel
-app.get('/', (_, res) => {
-  res.redirect('/admin')
+// set trust proxy to true if you use nginx
+// when NodeJS app are served behind nginx reverse proxies and similar.
+// after that you must config your nginx server
+/*
+server {
+    listen 80;
+    server_name yourdomain.com;
+
+    location / {
+        proxy_pass http://localhost:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_cache_bypass $http_upgrade;
+    }
+}*/
+//app.set('trust proxy', true)
+
+// Dodaj middleware, który odczyta dane JSON z ciała żądania
+app.use(express.json())
+
+app.post('/cashbill-payment', async (req, res) => {
+  try {
+    // Odczytaj dane z ciała żądania
+    const requestData = req.body
+    //  console.log(requestData)
+    // Tutaj możesz użyć danych przekazanych z frontendu
+    const response = await axios.post(
+      'https://pay.cashbill.pl/testws/rest/payment/grzybole.pl',
+      requestData,
+    )
+    //   console.log(response)
+    res.status(200).json(response.data)
+  } catch (error: unknown) {
+    // console.error('Error:', error.message)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+})
+
+app.post('/send-email', async (req, res) => {
+  try {
+    // Odczytaj dane z ciała żądania
+    const requestData = req.body
+    await payload.sendEmail({
+      to: 'muczaczos@gmail.com',
+      from: 'shop@planet-of-mushrooms.com',
+      subject: 'New Message from: ' + requestData.name,
+      html: 'Email: ' + requestData.email + ' Content: ' + requestData.message,
+    })
+  } catch (error: unknown) {
+    // console.error('Error:', error.message)
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
 })
 
 const start = async (): Promise<void> => {
@@ -36,8 +89,31 @@ const start = async (): Promise<void> => {
     process.exit()
   }
 
-  app.listen(PORT, async () => {
-    payload.logger.info(`App URL: ${process.env.PAYLOAD_PUBLIC_SERVER_URL}`)
+  if (process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
+      payload.logger.info(`Next.js is now building...`)
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, '../'))
+      process.exit()
+    })
+
+    return
+  }
+
+  const nextApp = next({
+    dev: process.env.NODE_ENV !== 'production',
+  })
+
+  const nextHandler = nextApp.getRequestHandler()
+
+  app.use((req, res) => nextHandler(req, res))
+
+  nextApp.prepare().then(() => {
+    payload.logger.info('Starting Next.js...')
+
+    app.listen(PORT, async () => {
+      payload.logger.info(`Next.js App URL: ${process.env.PAYLOAD_PUBLIC_SERVER_URL}`)
+    })
   })
 }
 
