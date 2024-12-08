187db44e47911132a8745ea4d3be0e0a7016f473
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-12-08 17:00:06 +0100

    I changed server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import dotenv from 'dotenv'
 import next from 'next'
+import nextBuild from 'next/dist/build'
 import path from 'path'
 
 dotenv.config({
@@ -74,26 +75,6 @@ app.post('/send-email', async (req, res) => {
   }
 })
 
-app.get('/api/custom-posts', async (req, res) => {
-  try {
-    // Pobranie parametrów z zapytania
-    const { limit = 10, page = 1 } = req.query
-
-    // Wykonanie zapytania z parametrami limit i page
-    const posts = await payload.find({
-      collection: 'posts',
-      limit: parseInt(limit as string), // Konwersja limitu na liczbę
-      page: parseInt(page as string), // Konwersja strony na liczbę
-    })
-
-    // Zwrócenie wyników w odpowiedzi
-    res.json(posts)
-  } catch (error: unknown) {
-    // console.error('Error fetching posts:', error)
-    res.status(500).json({ message: 'Server error' })
-  }
-})
-
 const start = async (): Promise<void> => {
   await payload.init({
     secret: process.env.PAYLOAD_SECRET || '',
@@ -106,6 +87,17 @@ const start = async (): Promise<void> => {
   if (process.env.PAYLOAD_SEED === 'true') {
     await seed(payload)
     process.exit()
+  }
+
+  if (process.env.NEXT_BUILD) {
+    app.listen(PORT, async () => {
+      payload.logger.info(`Next.js is now building...`)
+      // @ts-expect-error
+      await nextBuild(path.join(__dirname, '../'))
+      process.exit()
+    })
+
+    return
   }
 
   const nextApp = next({
