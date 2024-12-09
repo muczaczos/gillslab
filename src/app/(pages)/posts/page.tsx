cef6b91db77589ed9980ab5d32743b469d169899
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-12-09 13:32:24 +0100

    I try to fix problem with useSearchParams on production

--- src/app/(pages)/posts/page.tsx
+++ src/app/(pages)/posts/page.tsx
@@ -20,24 +20,32 @@ const Posts = () => {
   const searchParams = useSearchParams()
 
   const getPosts = async (page: number, perPage: number) => {
-    const postsData = await fetchPosts(page, perPage)
-    setPosts(postsData.docs)
-    setTotalPages(postsData.totalPages)
+    try {
+      const postsData = await fetchPosts(page, perPage)
+      setPosts(postsData.docs)
+      setTotalPages(postsData.totalPages)
+    } catch (error) {
+      //console.error('Error fetching posts:', error)
+    }
   }
 
   useEffect(() => {
-    const pageFromUrl = searchParams.get('page')
-    const currentPageNum = pageFromUrl ? parseInt(pageFromUrl, 10) : 1
+    const fetchPostsOnLoad = () => {
+      const pageFromUrl = searchParams?.get('page')
+      const currentPageNum = pageFromUrl ? parseInt(pageFromUrl, 10) : 1
 
-    const savedShowAll = sessionStorage.getItem('showAll') === 'true'
-    setShowAll(savedShowAll)
-    setCurrentPage(currentPageNum)
+      const savedShowAll = sessionStorage.getItem('showAll') === 'true'
+      setShowAll(savedShowAll)
+      setCurrentPage(currentPageNum)
 
-    if (savedShowAll) {
-      getPosts(1, 9999)
-    } else {
-      getPosts(currentPageNum, 4)
+      if (savedShowAll) {
+        getPosts(1, 9999)
+      } else {
+        getPosts(currentPageNum, 4)
+      }
     }
+
+    fetchPostsOnLoad()
   }, [searchParams])
 
   const handlePageChange = (newPage: number) => {
@@ -80,78 +88,78 @@ const Posts = () => {
   }
 
   return (
-    <Suspense fallback={<div>Loading posts...</div>}>
-      <LayoutWithHeaderFooter>
-        <div className="pr-2 pl-2 bg-customWhite w-full flex flex-col items-center">
-          <div className="w-full flex flex-col mt-10 mb-2 md:max-w-[1536px]">
-            <h1 className="text-primary font-medium">Blog</h1>
-            <h2 className="text-primary-light font-normal text-2xl">Discover new articles</h2>
-          </div>
-
-          <div className="flex justify-center mt-5">
-            {showAll ? (
-              <button
-                onClick={handleReturnToPagination}
-                className="appearance-none border-none px-4 py-2 bg-primary text-customWhite rounded-lg hover:bg-primary-dark"
-              >
-                Return to Pagination
-              </button>
-            ) : (
-              <button
-                onClick={handleSeeAllToggle}
-                className="appearance-none border-none px-4 py-2 bg-primary text-customWhite rounded-lg hover:bg-primary-dark"
-              >
-                See All
-              </button>
-            )}
-          </div>
-
-          {!showAll && totalPages > 1 && (
-            <div className="flex w-full justify-between px-5 items-center md:max-w-[1536px]">
-              <div className="flex items-center justify-center mt-5 space-x-2">
-                <button
-                  className="appearance-none border-none bg-transparent text-xl text-customGray-dark disabled:text-customGray-light"
-                  onClick={() => handlePageChange(currentPage - 1)}
-                  disabled={currentPage === 1}
-                >
-                  {'<'}
-                </button>
-                {renderPageNumbers().map((page, index) =>
-                  typeof page === 'number' ? (
-                    <button
-                      key={index}
-                      className={`appearance-none border-none bg-customWhite px-3 py-1 text-customGray-dark ${
-                        page === currentPage
-                          ? 'bg-primary text-customWhite rounded-full'
-                          : 'hover:underline'
-                      }`}
-                      onClick={() => handlePageChange(page)}
-                    >
-                      {page}
-                    </button>
-                  ) : (
-                    <span key={index} className="text-customGray-light">
-                      {page}
-                    </span>
-                  ),
-                )}
-                <button
-                  className="appearance-none border-none bg-transparent text-xl text-customGray-dark disabled:text-customGray-light"
-                  onClick={() => handlePageChange(currentPage + 1)}
-                  disabled={currentPage === totalPages}
-                >
-                  {'>'}
-                </button>
-              </div>
-            </div>
-          )}
-
-          <PostsCards posts={posts} />
-
-          <div className="mb-40"></div>
+    <LayoutWithHeaderFooter>
+      <div className="pr-2 pl-2 bg-customWhite w-full flex flex-col items-center">
+        <div className="w-full flex flex-col mt-10 mb-2 md:max-w-[1536px]">
+          <h1 className="text-primary font-medium">Blog</h1>
+          <h2 className="text-primary-light font-normal text-2xl">Discover new articles</h2>
         </div>
-      </LayoutWithHeaderFooter>
-    </Suspense>
+
+        <div className="flex justify-center mt-5">
+          {showAll ? (
+            <button
+              onClick={handleReturnToPagination}
+              className="appearance-none border-none px-4 py-2 bg-primary text-customWhite rounded-lg hover:bg-primary-dark"
+            >
+              Return to Pagination
+            </button>
+          ) : (
+            <button
+              onClick={handleSeeAllToggle}
+              className="appearance-none border-none px-4 py-2 bg-primary text-customWhite rounded-lg hover:bg-primary-dark"
+            >
+              See All
+            </button>
+          )}
+        </div>
+
+        {!showAll && totalPages > 1 && (
+          <div className="flex w-full justify-between px-5 items-center md:max-w-[1536px]">
+            <div className="flex items-center justify-center mt-5 space-x-2">
+              <button
+                className="appearance-none border-none bg-transparent text-xl text-customGray-dark disabled:text-customGray-light"
+                onClick={() => handlePageChange(currentPage - 1)}
+                disabled={currentPage === 1}
+              >
+                {'<'}
+              </button>
+              {renderPageNumbers().map((page, index) =>
+                typeof page === 'number' ? (
+                  <button
+                    key={index}
+                    className={`appearance-none border-none bg-customWhite px-3 py-1 text-customGray-dark ${
+                      page === currentPage
+                        ? 'bg-primary text-customWhite rounded-full'
+                        : 'hover:underline'
+                    }`}
+                    onClick={() => handlePageChange(page)}
+                  >
+                    {page}
+                  </button>
+                ) : (
+                  <span key={index} className="text-customGray-light">
+                    {page}
+                  </span>
+                ),
+              )}
+              <button
+                className="appearance-none border-none bg-transparent text-xl text-customGray-dark disabled:text-customGray-light"
+                onClick={() => handlePageChange(currentPage + 1)}
+                disabled={currentPage === totalPages}
+              >
+                {'>'}
+              </button>
+            </div>
+          </div>
+        )}
+
+        <Suspense fallback={<div>Loading posts...</div>}>
+          <PostsCards posts={posts} />
+        </Suspense>
+
+        <div className="mb-40"></div>
+      </div>
+    </LayoutWithHeaderFooter>
   )
 }
 
