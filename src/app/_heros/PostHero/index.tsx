ff68fda5f9072cc257a80c87a4e4fc9dc7fda8ad
Arkadiusz Szatkowski <muczaczos@gmail.com>
2024-12-10 14:54:35 +0100

    I added circles with heart and share to post hero

--- src/app/_heros/PostHero/index.tsx
+++ src/app/_heros/PostHero/index.tsx
@@ -1,62 +1,57 @@
-import React, { Fragment } from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
+import React from 'react'
+import { FaHeart } from 'react-icons/fa'
+import { PiShareFatFill } from 'react-icons/pi'
 
 import { Post } from '../../../payload/payload-types'
-import { Gutter } from '../../_components/Gutter'
-import { Media } from '../../_components/Media'
-import RichText from '../../_components/RichText'
-import { formatDateTime } from '../../_utilities/formatDateTime'
-
-import classes from './index.module.scss'
 
 export const PostHero: React.FC<{
   post: Post
 }> = ({ post }) => {
-  const {
-    id,
-    title,
-    categories,
-    meta: { image: metaImage, description } = {},
-    publishedAt,
-    populatedAuthors,
-  } = post
+  const { title, meta: { image: metaImage } = {}, publishedAt } = post
 
   const serverURL = process.env.PAYLOAD_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
-  function formatDateWithComma(dateString) {
+  function formatDateWithComma(dateString: string) {
     const date = new Date(dateString)
 
-    // Pobieramy części daty
     const day = date.toLocaleDateString('en-GB', { day: '2-digit' })
     const month = date.toLocaleDateString('en-GB', { month: 'short' })
     const year = date.toLocaleDateString('en-GB', { year: 'numeric' })
 
-    // Składamy wynik z przecinkiem po miesiącu
     return `${day} ${month}, ${year}`
   }
 
-  var imageLink
+  let imageLink
   if (typeof post.meta.image === 'object' && post.meta.image !== null) {
     imageLink = serverURL + '/media/' + post.meta.image.filename
   }
 
   return (
-    <div className="p-3 h-1/2">
+    <div className="p-3 h-1/2 relative">
       <div
-        className="rounded-3xl w-full h-full bg-center bg-no-repeat bg-fit bg-cover relative"
+        className="rounded-3xl w-full h-full bg-center bg-no-repeat bg-cover relative"
         style={{
           backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0)), url(${imageLink})`,
         }}
       >
         <div className="flex h-full items-end px-4 pb-10">
-          {/* Warstwa przyciemniająca */}
-          <div className="flex flex-col">
-            <h1 className="text-customWhite text-2xl z-10">{post.title}</h1>
+          <div className="flex flex-col z-10">
+            <h1 className="text-customWhite text-2xl">{post.title}</h1>
             <p className="text-gray-300 text-sm pt-2">
               {formatDateWithComma(post.publishedAt)} <span className="text-customWhite">•</span>{' '}
               Categories
             </p>
+          </div>
+        </div>
+
+        {/* Kółeczka w prawym dolnym rogu */}
+        <div className="absolute bottom-[-20px] right-5 flex space-x-4 z-20">
+          <div className="w-12 h-12 bg-white rounded-full shadow-lg flex items-center justify-center">
+            <PiShareFatFill className="text-customGray-light text-2xl" />
+          </div>
+
+          <div className="w-12 h-12 bg-white rounded-full shadow-lg flex items-center justify-center">
+            <FaHeart className="text-secondary text-2xl" />
           </div>
         </div>
       </div>
