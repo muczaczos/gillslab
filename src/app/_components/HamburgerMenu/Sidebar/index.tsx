b02a4a3afeedffbf1e40c13e9ba307e8225c7f66
Arkadiusz Szatkowski <muczaczos@gmail.com>
2025-01-09 15:40:36 +0100

    I added style to hamburger menu

---
+++ src/app/_components/HamburgerMenu/Sidebar/index.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import Link from 'next/link'
+
+const Sidebar = () => {
+  const [isShopOpen, setIsShopOpen] = useState(false)
+
+  const toggleShopMenu = () => {
+    setIsShopOpen(!isShopOpen)
+  }
+
+  return (
+    <div className="w-5/6 pt-20 px-5 h-full bg-transparent z-50">
+      {/* Shop section */}
+      <div
+        onClick={toggleShopMenu} className="pt-1 flex items-center justify-between">
+        <p
+          onClick={toggleShopMenu}
+          className="text-customWhite font-semibold text-xl block pb-3 cursor-pointer"
+        >
+          Shop
+        </p>
+        <div
+          onClick={toggleShopMenu}
+          className="text-customWhite font-semibold text-xl pb-3 cursor-pointer bg-transparent"
+        >
+          {isShopOpen ? '-' : '+'}
+        </div>
+      </div>
+      {isShopOpen && (
+        <div className="pl-4">
+          <Link href="/shop/category1">
+            <p className="text-customWhite font-medium text-lg pb-2 cursor-pointer">Category 1</p>
+          </Link>
+          <Link href="/shop/category2">
+            <p className="text-customWhite font-medium text-lg pb-2 cursor-pointer">Category 2</p>
+          </Link>
+          <Link href="/shop/category3">
+            <p className="text-customWhite font-medium text-lg pb-2 cursor-pointer">Category 3</p>
+          </Link>
+          <Link href="/shop/category4">
+            <p className="text-customWhite font-medium text-lg pb-2 cursor-pointer">Category 4</p>
+          </Link>
+        </div>
+      )}
+
+      {/* Other sections */}
+      <Link href="/contact">
+        <div className="pt-1">
+          <p className="text-customWhite font-semibold text-xl block pb-3">Contact</p>
+        </div>
+      </Link>
+      <Link href="/conditions">
+        <div className="pt-1">
+          <p className="text-customWhite font-semibold text-xl block pb-3">Conditions</p>
+        </div>
+      </Link>
+      <Link href="/privacy">
+        <div className="pt-1">
+          <p className="text-customWhite font-semibold text-xl block pb-3">Privacy</p>
+        </div>
+      </Link>
+      <Link href="/payment">
+        <div className="pt-1">
+          <p className="text-customWhite font-semibold text-xl block pb-3">Payment</p>
+        </div>
+      </Link>
+      <Link href="/shipping">
+        <div className="pt-1">
+          <p className="text-customWhite font-semibold text-xl block pb-3">Shipping</p>
+        </div>
+      </Link>
+      <Link href="/blog">
+        <div className="pt-1">
+          <p className="text-customWhite font-semibold text-xl block pb-3">Blog</p>
+        </div>
+      </Link>
+      <Link href="/vlog">
+        <div className="pt-1">
+          <p className="text-customWhite font-semibold text-xl block pb-3">Vlog</p>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default Sidebar
--- src/app/_components/HamburgerMenu/index.tsx
+++ src/app/_components/HamburgerMenu/index.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react'
 import { HiMenu, HiX } from 'react-icons/hi' // Import ikon z Heroicons
 import Link from 'next/link'
 
+import Sidebar from './Sidebar'
+
 export default function HamburgerMenu() {
   const [isOpen, setIsOpen] = useState(false)
   const [isClosing, setIsClosing] = useState(false)
@@ -45,92 +47,15 @@ export default function HamburgerMenu() {
       {/* Menu po otwarciu i zamykaniu */}
       {(isOpen || isClosing) && (
         <div
-          className={`fixed inset-0 bg-primary flex flex-col justify-center items-center ${
-            isClosing ? 'animate-slideUp' : 'animate-slideDown'
-          }`}
+          className={`fixed flex inset-0 bg-primary ${isClosing ? 'animate-slideUp' : 'animate-slideDown'
+            }`}
         >
           <div
             className="absolute inset-0 bg-no-repeat bg-cover opacity-5"
             style={{ backgroundImage: "url('/media/eyes.png')" }}
           ></div>
-          <div className="flex justify-center gap-2 p-4 text-left z-50 w-full">
-            <div>
-              <p className="text-customWhite font-black text-3xl underline">Info</p>
-              <Link className="" href="/vlog">
-                <div className="pt-1">
-                  <p className="text-customWhite font-bold text-2xl block pb-3">Vlog</p>
-                </div>
-              </Link>
-              <Link className="" href="/blog">
-                <div className="pt-1">
-                  <p className="text-customWhite font-bold text-2xl block pb-3">Blog</p>
-                </div>
-              </Link>
-              <Link href="/about">
-                <div className=" pt-1">
-                  <p className="text-customWhite font-bold text-2xl block pb-3">About</p>
-                </div>
-              </Link>
-              <Link className="" href="/privacy">
-                <div className=" pt-1">
-                  <p className="text-customWhite font-bold text-2xl block pb-3">Privacy</p>
-                </div>
-              </Link>
-              <Link className="" href="/contact">
-                <div className="pt-1">
-                  <p className="text-customWhite font-bold text-2xl block pb-3">Contact</p>
-                </div>
-              </Link>
-              <Link className="" href="/shipping">
-                <div className="pt-1">
-                  <p className="text-customWhite font-bold text-2xl block pb-3">Shipping</p>
-                </div>
-              </Link>
-              <Link className="" href="/payments">
-                <div className="pt-1">
-                  <p className="text-customWhite font-bold text-2xl block pb-3">Payments</p>
-                </div>
-              </Link>
-              <Link className="" href="/conditions">
-                <div className=" pt-1">
-                  <p className="text-customWhite font-bold text-2xl block pb-3">Conditions</p>
-                </div>
-              </Link>
-            </div>
-            <div>
-              <p className="text-customWhite font-black text-3xl underline">Products</p>
-              <Link className="" href="/laboratory-equipments">
-                <div className="pt-1">
-                  <p className="text-customWhite font-bold text-2xl block pb-3">Lab</p>
-                </div>
-              </Link>
-              <Link className="" href="/cubensis-spore-syringes">
-                <div className="pt-1">
-                  <p className="text-customWhite font-bold text-2xl block pb-3">Spores</p>
-                </div>
-              </Link>
-              <Link className="" href="/cubensis-liquid-cultures">
-                <div className="pt-1">
-                  <p className="text-customWhite font-bold text-2xl block pb-3">Cultures</p>
-                </div>
-              </Link>
-              <Link className="" href="/cubensis-grow-kits">
-                <div className="pt-1">
-                  <p className="text-customWhite font-bold text-2xl block pb-3">Growkits</p>
-                </div>
-              </Link>
-              <Link className="" href="/favorities">
-                <div className="pt-1">
-                  <p className="text-customWhite font-bold text-2xl block pb-3">Favorities</p>
-                </div>
-              </Link>
-              <Link className="" href="/substrates">
-                <div className="pt-1">
-                  <p className="text-customWhite font-bold text-2xl block pb-3">Substrates</p>
-                </div>
-              </Link>
-            </div>
-          </div>
+          <div className="w-1/6 h-full bg-secondary"></div>
+          <Sidebar />
         </div>
       )}
     </div>
