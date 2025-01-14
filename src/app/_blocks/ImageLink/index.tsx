ac1c9fc4681ffbee306a9218fd9cf5537a81d781
Arkadiusz Szatkowski <muczaczos@gmail.com>
2025-01-14 16:01:51 +0100

    Shop page allmost done

--- src/app/_blocks/ImageLink/index.tsx
+++ src/app/_blocks/ImageLink/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image, { StaticImageData } from 'next/image'
+import Link from 'next/link'
 
 import { Page } from '../../../payload/payload-types'
 import { Gutter } from '../../_components/Gutter'
@@ -8,7 +9,7 @@ import RichText from '../../_components/RichText'
 
 import classes from './index.module.scss'
 
-type Props = Extract<Page['layout'][0], { blockType: 'text' }> & {
+type Props = Extract<Page['layout'][0], { blockType: 'imageLink' }> & {
   staticImage?: StaticImageData
   id?: string
   test?: string
@@ -16,88 +17,38 @@ type Props = Extract<Page['layout'][0], { blockType: 'text' }> & {
 }
 
 export const ImageLink: React.FC<Props> = props => {
+  const { items } = props // Wyciągamy tablicę `items` z propsów
+
+  console.log(items[0].media.url)
+
   return (
     <Gutter className="pb-20 justify-center flex flex-wrap">
-      <div className="m-5 max-w-sm rounded-xl overflow-hidden shadow-lg border border-gray-200 bg-white">
-        {/* Obrazek */}
-        <Image
-          className="p-2 w-[100px] h-[100px]"
-          src="/media/promotions.png"
-          alt="Example"
-          height="100"
-          width="100"
-        />
-        {/* Treść */}
-        <div className="px-4 pb-4">
-          {/* Tytuł */}
-          <h2 className="text-lg font-bold text-gray-800 mb-2">Przykładowy Tytuł</h2>
-          {/* Treść */}
-          <p className="text-gray-600">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-          </p>
-        </div>
-      </div>
-      <div className="m-5 max-w-sm rounded-xl overflow-hidden shadow-lg border border-gray-200 bg-white">
-        {/* Obrazek */}
-        <Image
-          className="p-2 w-[100px] h-[100px]"
-          src="/media/promotions.png"
-          alt="Example"
-          height="100"
-          width="100"
-        />
-        {/* Treść */}
-        <div className="px-4 pb-4">
-          {/* Tytuł */}
-          <h2 className="text-lg font-bold text-gray-800 mb-2">Przykładowy Tytuł</h2>
-          {/* Treść */}
-          <p className="text-gray-600">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-          </p>
-        </div>
-      </div>
-      <div className="m-5 max-w-sm rounded-xl overflow-hidden shadow-lg border border-gray-200 bg-white">
-        {/* Obrazek */}
-        <Image
-          className="p-2 w-[100px] h-[100px]"
-          src="/media/promotions.png"
-          alt="Example"
-          height="100"
-          width="100"
-        />
-        {/* Treść */}
-        <div className="px-4 pb-4">
-          {/* Tytuł */}
-          <h2 className="text-lg font-bold text-gray-800 mb-2">Przykładowy Tytuł</h2>
-          {/* Treść */}
-          <p className="text-gray-600">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-          </p>
-        </div>
-      </div>
-      <div className="m-5 max-w-sm rounded-xl overflow-hidden shadow-lg border border-gray-200 bg-white">
-        {/* Obrazek */}
-        <Image
-          className="p-2 w-[100px] h-[100px]"
-          src="/media/promotions.png"
-          alt="Example"
-          height="100"
-          width="100"
-        />
-        {/* Treść */}
-        <div className="px-4 pb-4">
-          {/* Tytuł */}
-          <h2 className="text-lg font-bold text-gray-800 mb-2">Przykładowy Tytuł</h2>
-          {/* Treść */}
-          <p className="text-gray-600">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-          </p>
-        </div>
-      </div>
+      {items &&
+        items.map((item, index) => (
+          <Link
+            key={index}
+            href={item.url}
+            className="m-5 max-w-sm rounded-xl overflow-hidden shadow-lg border border-gray-200 bg-white"
+          >
+            {/* Obrazek */}
+            {item.media && typeof item.media !== 'string' && (
+              <Image
+                className="p-2 w-[150px] h-[150px]"
+                src={item.media.url}
+                alt="Example"
+                height="100"
+                width="100"
+              />
+            )}
+            {/* Treść */}
+            <div className="px-4 pb-4">
+              {/* Tytuł */}
+              <h2 className="text-lg font-bold text-customGray-dark mb-2">{item.title}</h2>
+              {/* Treść */}
+              <RichText content={item.richText} className="" />{' '}
+            </div>
+          </Link>
+        ))}
     </Gutter>
   )
 }
