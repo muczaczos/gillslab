631d2684ad32ea9779882b415df2e38c76afa877
Arkadiusz Szatkowski <muczaczos@gmail.com>
2025-03-26 16:50:06 +0100

    I added best products without links

--- src/app/(pages)/[slug]/BestProducts/index.tsx
+++ src/app/(pages)/[slug]/BestProducts/index.tsx
@@ -19,65 +19,62 @@ const BestProducts = () => {
             >
               <div className=" xl:flex xl:gap-12">
                 <div className="mb-2 relative flex flex-col justify-aroun bg-customWhite shadow-xl rounded-xl h-[12rem] w-[10rem] sm:h-[16rem] sm:w-[14rem]">
-                  <FaHeart className="absolute top-3 right-3 text-secondary text-2xl" />
+                  <p className="absolute pl-3 text-primary text-2xl font-bold top-3 z-[100]">B+</p>
                   <div className="mt-5 flex justify-center">
                     <Image
-                      src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/mckLabel.png`}
+                      src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/b_liquid_fotor.png`}
                       alt="Promotion"
-                      className="w-[5rem] py-4 sm:w-[7rem] sm:mt-[1rem]"
+                      className="w-[5rem] sm:w-[7rem] sm:mt-[1rem]"
                       width={100}
                       height={100}
                     />
                   </div>
-                  <p className="leading-3 pl-3 text-primary font-bold opacity-70">Cambodian</p>
-                  <p className="text-primary text-2xl font-bold pl-3">$35</p>
+                  <p className="absolute bottom-3 text-primary text-2xl font-bold pl-3">$35</p>
                 </div>
 
                 <div className="mb-2 relative flex flex-col justify-aroun bg-customWhite shadow-xl rounded-xl h-[12rem] w-[10rem] sm:h-[16rem] sm:w-[14rem]">
-                  <FaHeart className="absolute top-3 right-3 text-secondary text-2xl" />
-                  <div className="mt-5 flex justify-center">
+                  <p className="absolute pl-3 text-primary text-2xl font-bold top-3">Cambodian</p>
+                  <div className="flex justify-center">
                     <Image
-                      src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/ecuLabel.png`}
+                      src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/Cambodia_monotub_transparent.png`}
                       alt="Promotion"
-                      className="w-[5rem] py-4 sm:w-[7rem] sm:mt-[1rem]"
-                      width={100}
-                      height={100}
+                      className="w-[10rem] h-[10rem] sm:w-[12rem] sm:h-[12rem] sm:mt-[1rem]"
+                      width={200}
+                      height={200}
                     />
                   </div>
-                  <p className="leading-3 pl-3 text-primary font-bold opacity-70">Cambodian</p>
-                  <p className="text-primary text-2xl font-bold pl-3">$35</p>
+                  <p className="absolute bottom-3 text-primary text-2xl font-bold pl-3">$50</p>
                 </div>
               </div>
 
               <div className="xl:flex xl:gap-12">
                 <div className="mb-2 relative flex flex-col justify-aroun bg-customWhite shadow-xl rounded-xl h-[12rem] w-[10rem] sm:h-[16rem] sm:w-[14rem]">
-                  <FaHeart className="absolute top-3 right-3 text-secondary text-2xl" />
-                  <div className="mt-5 flex justify-center">
+                  <p className="absolute pl-3 text-primary text-2xl font-bold top-3">Golden T.</p>
+                  <div className="mt-2 flex justify-center">
                     <Image
-                      src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/gtLabel.png`}
+                      src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/Golden_plate-fotor-1.png`}
                       alt="Promotion"
-                      className="w-[5rem] py-4 sm:w-[7rem] sm:mt-[1rem]"
+                      className="w-[10rem] sm:w-[12rem] sm:mt-[1rem]"
                       width={100}
                       height={100}
                     />
                   </div>
-                  <p className="leading-3 pl-3 text-primary font-bold opacity-70">Cambodian</p>
-                  <p className="text-primary text-2xl font-bold pl-3">$35</p>
+                  <p className="absolute bottom-3 text-primary text-2xl font-bold pl-3">$20</p>
                 </div>
 
                 <div className="mb-2 relative flex flex-col justify-aroun bg-customWhite shadow-xl rounded-xl h-[12rem] w-[10rem] sm:h-[16rem] sm:w-[14rem]">
-                  <FaHeart className="absolute top-3 right-3 text-secondary text-2xl" />
-                  <div className="mt-5 flex justify-center">
+
+                  <p className="absolute pl-3 text-primary text-2xl font-bold top-3">Malabar</p>
+                  <div className="mt-1 flex justify-center">
                     <Image
-                      src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/colLabel.png`}
+                      src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/Malabar_print-fotor.png`}
                       alt="Promotion"
-                      className="w-[5rem] py-4 sm:w-[7rem] sm:mt-[1rem]"
+                      className="w-[10rem] sm:w-[12rem] sm:mt-[1rem]"
                       width={100}
                       height={100}
                     />
                   </div>
-                  <p className="leading-3 pl-3 text-primary font-bold opacity-70">Cambodian</p>
-                  <p className="text-primary text-2xl font-bold pl-3">$35</p>
+                  <p className="absolute bottom-3 pl-3 text-primary text-2xl font-bold">$25</p>
                 </div>
               </div>
             </div>
