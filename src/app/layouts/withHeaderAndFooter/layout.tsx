7c79557a56da9756297b55c91783117c380d3e70
Arkadiusz Szatkowski <muczaczos@gmail.com>
2025-04-17 16:01:44 +0200

    I restored previous version of layoutwithheaderFooter

--- src/app/layouts/withHeaderAndFooter/layout.tsx
+++ src/app/layouts/withHeaderAndFooter/layout.tsx
@@ -1,19 +1,14 @@
 import React from 'react'
 
-import { fetchFooter } from '../../_api/fetchGlobals'
 import { Footer } from '../../_components/Footer'
-import FooterComponent from '../../_components/Footer/FooterComponent'
 import { Header } from '../../_components/Header' //
 
-export default async function LayoutWithHeaderFooter({ children }: { children: React.ReactNode }) {
-  const footerData = await fetchFooter()
-
+export default function LayoutWithHeaderFooter({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
       <main>{children}</main>
-
-      <FooterComponent footer={footerData} />
+      <Footer />
     </>
   )
 }
