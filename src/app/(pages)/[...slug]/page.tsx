8a364cf348651692535aaadf3139edb7d0d253e1
Arkadiusz Szatkowski <muczaczos@gmail.com>
2025-04-17 15:58:51 +0200

    I deleted LayoutWithHeader form main page

--- src/app/(pages)/[...slug]/page.tsx
+++ src/app/(pages)/[...slug]/page.tsx
@@ -64,7 +64,7 @@ export default async function Pages({ params }) {
   const { hero, layout } = page
 
   return (
-    <LayoutWithHeaderFooter>
+    <>
       {finalSlug === 'home' ? (
         <>
           <DesktopHero />
@@ -83,7 +83,7 @@ export default async function Pages({ params }) {
           />
         </>
       )}
-    </LayoutWithHeaderFooter>
+    </>
   )
 }
 
