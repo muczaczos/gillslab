7adbdb1aebcfba2cf2636a376038771630a23904
Arkadiusz Szatkowski <muczaczos@gmail.com>
2025-04-17 15:37:21 +0200

    Load footer without use effect for checking performance

--- src/app/layouts/withHeaderAndFooter/layout.tsx
+++ src/app/layouts/withHeaderAndFooter/layout.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
+import { fetchFooter } from '../../_api/fetchGlobals'
 import { Footer } from '../../_components/Footer'
+import FooterComponent from '../../_components/Footer/FooterComponent'
 import { Header } from '../../_components/Header' //
 
-export default function LayoutWithHeaderFooter({ children }: { children: React.ReactNode }) {
+export default async function LayoutWithHeaderFooter({ children }: { children: React.ReactNode }) {
+  const footerData = await fetchFooter()
+
   return (
     <>
       <Header />
       <main>{children}</main>
-      <Footer />
+
+      <FooterComponent footer={footerData} />
     </>
   )
 }
