75e8b53203cdaa02359bf5257fb3d4e76eb53e74
Arkadiusz Szatkowski <muczaczos@gmail.com>
2025-04-17 16:27:10 +0200

    I delete 'use client' directive from FooterComponent

--- src/app/_components/Footer/FooterComponent/index.tsx
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import {
@@ -24,30 +23,37 @@ import { Input } from '../../Input'
 import classes from './index.module.scss'
 
 const FooterComponent = ({ footer }: { footer: Footer }) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isLoading },
-  } = useForm<FormData>()
-  const router = useRouter()
-  const [email, setEmailValue] = useState('')
-  const pathname = usePathname()
-  const navItems = footer?.navItems || []
-  const handleAddress = e => {
-    setEmailValue(e.target.value)
-  }
-  const handleSubscriber = async () => {
-    router.push(`/subscribe?email=${email}`)
-  }
-
   return (
     <>
-      <footer className={`${noHeaderFooterUrls.includes(pathname) ? classes.hide : ''} relative`}>
+      <footer className={`relative`}>
         {/*Footer image*/}
-        <div className="bg-transparent md:hidden -mt-40 relative overflow-hidden h-auto"></div>
-        <div className="bg-customWhite hidden md:block md:mb-10 xxl:hidden"></div>
+        <div className="bg-transparent md:hidden -mt-40 relative overflow-hidden h-auto">
+          <Image
+            src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/mobFoot3.png`}
+            alt="Example Image"
+            width="2000"
+            height="200"
+            objectFit="cover"
+            className="relative left-0"
+          />
+        </div>
+        <div className="bg-customWhite hidden md:block md:mb-10 xxl:hidden">
+          <Image
+            src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/bigFoot.png`}
+            alt="Example Image"
+            width="2000"
+            height="200"
+            objectFit="cover"
+            className="relative left-0"
+          />
+        </div>
 
-        <div className="xs:-mt-16 -mt-20 bg-cover bg-primary relative z-20 p-5 xs:p-3 xxl:pt-20">
+        <div
+          className="xs:-mt-16 -mt-20 bg-cover bg-primary relative z-20 p-5 xs:p-3 xxl:pt-20"
+          style={{
+            backgroundImage: `url(${process.env.NEXT_PUBLIC_SERVER_URL}/media/eyes5.png)`,
+          }}
+        >
           <div className="">
             <div className="w-full xxl:w-[2000px] xxl:mx-auto">
               {/* Address, Socials, Payments, Shipping*/}
@@ -56,6 +62,14 @@ const FooterComponent = ({ footer }: { footer: Footer }) => {
                   {/* Logo and company details */}
                   <div className="flex flex-col lg:flex-row lg:gap-5">
                     <section className="mb-5">
+                      <Image
+                        src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/logo3.png`}
+                        alt="Gillslab logo"
+                        width="150"
+                        height="100"
+                        objectFit="cover"
+                        className="relative -left-[10px]"
+                      />
                       <p className="text-customWhite text-xl">Gillslab LTD.</p>
                       <p className="text-customWhite text-xl">05-500</p>
                       <p className="text-customWhite text-xl">Piaseczno</p>
@@ -151,6 +165,63 @@ const FooterComponent = ({ footer }: { footer: Footer }) => {
           </div>
           <hr className="border-customWhite w-full sm:mb-10 xl:w-3/4" />
           {/* dhl, dpd, ups, fedex logotypes */}
+          <div className="flex items-center gap-2 w-full h-24 mx-auto overflow-hidden lg:max-w-[1024px] 2xl:max-w-[1536px]">
+            {/* Kontener z 50% szerokości i wyśrodkowany */}
+            <div className="relative w-full h-16">
+              {' '}
+              {/* Ustawienie stałej wysokości */}
+              <Image
+                src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/dhl.png`}
+                alt="dhl logo image"
+                className="object-contain w-full h-full" // Ustawienie w pełni skalowalnego obrazu
+                width={100}
+                height={100}
+              />
+            </div>
+            <div className="relative w-full h-16">
+              <Image
+                src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/dpd.png`}
+                alt="dpd logo image"
+                className="object-contain w-full h-full" // Ustawienie w pełni skalowalnego obrazu
+                width={100}
+                height={100}
+              />
+            </div>
+            <div className="relative w-full h-16">
+              <Image
+                src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/ups.png`}
+                alt="ups logo image"
+                className="object-contain w-full h-full" // Ustawienie w pełni skalowalnego obrazu
+                width={100}
+                height={100}
+              />
+            </div>
+            <div className="relative w-full h-16">
+              <Image
+                src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/fedex.png`}
+                alt="fedex logo image"
+                className="object-contain w-full h-full" // Ustawienie w pełni skalowalnego obrazu
+                width={100}
+                height={100}
+              />
+            </div>
+          </div>
+
+          {/* Gillslab logo and date */}
+          <div className="w-full flex justify-center">
+            <div>
+              <div className="mt-2 lg:mt-10 mb-20 items-center flex gap-2 lg:w-[1000px] 2xl:w-[1400px]">
+                <p className="text-customWhite text-large font-bold">© 2008 - 2024</p>
+                <Image
+                  src={`${process.env.NEXT_PUBLIC_SERVER_URL}/media/logoCom.png`}
+                  alt="Gillslab logo"
+                  className="" // Skaluje obrazek do 50% jego rozmiarów
+                  width={150}
+                  height={100}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </footer>
     </>
