7de4caad88a4e1a4b40fa57b12c4d49c4698d9c4
Arkadiusz Szatkowski <muczaczos@gmail.com>
2025-06-19 22:32:25 +0200

    Final deploy test

--- src/app/(pages)/[...slug]/page.tsx
+++ src/app/(pages)/[...slug]/page.tsx
@@ -75,13 +75,13 @@ export default async function Pages({ params }) {
           <BestProducts />
           <BlogVlogNews />
           {/* --- DYNAMICZNA TREŚĆ Z DASHBOARDU --- */}
+          {/*
           <Hero {...hero} />
           <Blocks
             blocks={layout}
             disableTopPadding={!hero || hero?.type === 'none' || hero?.type === 'lowImpact'}
           />
-          <Banners />
-          <Banners />
+          */}
         </>
       ) : (
         <>
